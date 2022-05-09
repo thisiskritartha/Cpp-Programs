8900887cfc370cbc968e1164b25a5549550fc231
Kritartha Bikram Shah <67311836+thisiskritartha@users.noreply.github.com>
2022-05-09 11:24:18 +0545

    Max till i'th term.
    Eg :->                  1 0 5 4 6 8
    max till ith term:  1 1 5 5 6 8

---
+++ maxTillLastTerm.cpp
@@ -0,0 +1,20 @@
+//find the max term till i'th term.
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int32_t main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+    int mx = INT_MIN;
+    for(int i=0; i<n; i++){
+        mx = max(mx, arr[i]);
+        cout<<mx<<" ";
+    }
+    return 0;
+}
